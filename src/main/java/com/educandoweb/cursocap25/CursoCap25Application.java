02c8e90edc307f0070ce935933f4e43efc284f61
Hilton Magno <hdmagno@gmail.com>
2020-04-25 19:05:59 -0300

    Project created

---
+++ src/main/java/com/educandoweb/cursocap25/CursoCap25Application.java
@@ -0,0 +1,13 @@
+package com.educandoweb.cursocap25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoCap25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoCap25Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/cursocap25/CursoCap25ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.educandoweb.cursocap25;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoCap25ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
