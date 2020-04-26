9f7c0ab37fbc81c9791d7226dd5ae57767a287de
Hilton Magno <hdmagno@gmail.com>
2020-04-26 11:05:15 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/educandoweb/coursecap25/repositories/OrderRepository.java
@@ -0,0 +1,17 @@
+package com.educandoweb.coursecap25.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.coursecap25.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+	
+
+	
+
+	
+
+	
+
+}
