944e6b51b859ef94ce1448037d2c7da8828d7002
Hilton Magno <hdmagno@gmail.com>
2020-04-26 13:08:40 -0300

    Product

---
+++ src/main/java/com/educandoweb/coursecap25/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.educandoweb.coursecap25.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.coursecap25.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	
+
+	
+	
+
+}
