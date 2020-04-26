531a283984c935306d5501ca2b51832e9c0f2e55
Hilton Magno <hdmagno@gmail.com>
2020-04-26 12:22:24 -0300

    Category

--- src/main/java/com/educandoweb/coursecap25/config/TestConfig.java
+++ src/main/java/com/educandoweb/coursecap25/config/TestConfig.java
@@ -1,5 +1,4 @@
 package com.educandoweb.coursecap25.config;
-
 import java.time.Instant;
 import java.util.Arrays;
 
@@ -8,9 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.coursecap25.entities.Category;
 import com.educandoweb.coursecap25.entities.Order;
 import com.educandoweb.coursecap25.entities.User;
 import com.educandoweb.coursecap25.entities.enums.OrderStatus;
+import com.educandoweb.coursecap25.repositories.CategoryRepository;
 import com.educandoweb.coursecap25.repositories.OrderRepository;
 import com.educandoweb.coursecap25.repositories.UserRepository;
 
@@ -23,9 +24,19 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
---
+++ src/main/java/com/educandoweb/coursecap25/repositories/CategoryRepository.java
@@ -0,0 +1,14 @@
+package com.educandoweb.coursecap25.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.coursecap25.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+	
+
+	
+	
+
+}
